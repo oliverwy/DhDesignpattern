e28519c97c590c0ae93ac4c6ab252b94cdb7ea45
oliver <oliver@oliverdeMacBook-Air.local>
2018-09-16 00:01:50 +0800

    ch7 finish  commit

---
+++ src/ch7/proxy/Pursuit.java
@@ -0,0 +1,67 @@
+package ch7.proxy;
+
+interface IGiveGift
+{
+	void GiveDolls();
+	void GiveFlowers();
+	void GiveChocolate();
+}
+public class Pursuit implements IGiveGift{
+	SchoolGirl mm;
+
+	public Pursuit(SchoolGirl mm) {
+		super();
+		this.mm = mm;
+	}
+	public void GiveDolls()
+	{
+		System.out.println(mm.getName()+" 送你洋娃娃");
+	}
+	
+	public void GiveFlowers()
+	{
+		System.out.println(mm.getName()+" 送你鲜花");
+	}
+	
+	public void GiveChocolate()
+	{
+		System.out.println(mm.getName()+" 送你巧克力");
+	}
+	
+}
+
+class SchoolGirl{
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
+
+class Proxy implements IGiveGift{
+	Pursuit gg;
+	public Proxy(SchoolGirl mm) {
+		super();
+		gg=new Pursuit(mm);
+	}
+	public void GiveDolls()
+	{
+		gg.GiveDolls();;
+	}
+	
+	public void GiveFlowers()
+	{
+		gg.GiveFlowers();
+	}
+	
+	public void GiveChocolate()
+	{
+		gg.GiveChocolate();
+	}
+
+}
+
---
+++ src/ch7/proxynormal/Client.java
@@ -0,0 +1,11 @@
+package ch7.proxynormal;
+
+public class Client {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Subject t=new Proxy();
+		t.Requuest();
+	}
+
+}
