e39329dcaf023f6970b7475803b8fa7f6010e9f7
oliver <oliver@oliverdeMacBook-Air.local>
2018-09-16 00:12:46 +0800

    ch1 finish  commit

---
+++ src/ch1/protye/Operation.java
@@ -0,0 +1,27 @@
+package ch1.protye;
+
+
+public class Operation {
+    public static double getResult(double numberA,double numberB,String operate) {
+        double result=0;
+        switch(operate) {   
+        case "+":
+            result=numberA+numberB;
+            break;
+        case "-":
+            result=numberA-numberB;
+            break;
+        case "*":
+            result=numberA*numberB;
+            break;
+        case "/":
+            if(numberB!=0)
+                result=numberA/numberB;
+            else
+                System.out.println("除数不能为0！");
+            break;
+        }
+        return result;
+    }
+
+}
