4a4579345f0c6145c25c19016d4365231d437766
oliver <oliver@oliverdeMacBook-Air.local>
2018-09-16 22:10:36 +0800

    ch10 finish

---
+++ src/ch10/TemplateMethodCode/AbstractClass.java
@@ -0,0 +1,47 @@
+package ch10.TemplateMethodCode;
+
+public abstract class AbstractClass {
+	public abstract void PrimitiveOperation1();
+	public abstract void PrimitiveOperation2();
+	public void templateMethod()
+	{
+		PrimitiveOperation1();
+		PrimitiveOperation2();
+		System.out.println("");
+	}
+}
+
+class ConcreteClassA extends AbstractClass
+{
+
+	@Override
+	public void PrimitiveOperation1() {
+		// TODO Auto-generated method stub
+		System.out.println("具体类A方法1的实现");
+	}
+
+	@Override
+	public void PrimitiveOperation2() {
+		// TODO Auto-generated method stub
+		System.out.println("具体类A方法2的实现");
+	}
+	
+}
+
+class ConcreteClassB extends AbstractClass
+{
+
+	@Override
+	public void PrimitiveOperation1() {
+		// TODO Auto-generated method stub
+		System.out.println("具体类B方法1的实现");		
+	}
+
+	@Override
+	public void PrimitiveOperation2() {
+		// TODO Auto-generated method stub
+		System.out.println("具体类B方法2的实现");		
+
+	}
+	
+}
