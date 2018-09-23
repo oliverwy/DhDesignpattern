baf97b6e5eccb714bab06b3bcc4b6702647dcba7
oliver <oliver@oliverdeMacBook-Air.local>
2018-09-24 00:07:38 +0800

    ch18 mementopattern

---
+++ src/ch18/firstgamesaver/Client.java
@@ -0,0 +1,27 @@
+package ch18.firstgamesaver;
+
+public class Client {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		GameRole lixiaoyao=new GameRole();
+		lixiaoyao.getInitState();
+		lixiaoyao.stateDisplay();
+		
+		GameRole backup=new GameRole();
+		backup.setVit(lixiaoyao.getVit());
+		backup.setAtk(lixiaoyao.getAtk());
+		backup.setDef(lixiaoyao.getDef());
+		
+		lixiaoyao.fight();
+		
+		lixiaoyao.stateDisplay();
+		
+		lixiaoyao.setAtk(backup.getAtk());
+		lixiaoyao.setDef(backup.getDef());
+		lixiaoyao.setVit(backup.getVit());
+		
+		lixiaoyao.stateDisplay();
+	}
+
+}
---
+++ src/ch18/gamesavenormalmemento/RoleStateCaretaker.java
@@ -0,0 +1,13 @@
+package ch18.gamesavenormalmemento;
+
+public class RoleStateCaretaker {
+	private RoleStateMemento memento;
+	public RoleStateMemento getMemento() {
+		return memento;
+	}
+
+	public void setMemento(RoleStateMemento memento) {
+		this.memento = memento;
+	}
+
+}
